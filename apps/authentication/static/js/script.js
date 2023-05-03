10f65da52f23c20261ed9bade838cc9a22f5db47
GustavoBatistaDev <gustavobatistadev@gmail.com>
2023-05-03 15:08:07 -0300

    validando forms

--- apps/authentication/static/js/script.js
+++ apps/authentication/static/js/script.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+(function(){
+  $(document).ready(function() {
     $('.input-names').on('input', function() {
       // aqui você pode inserir a lógica de validação dos campos
       // por exemplo:
@@ -13,20 +14,43 @@ $(document).ready(function() {
 
   
   $(document).ready(function() {
-    $('.input-passwords').on('input', function() {
-    
-    //let regex = /^(?=.*[a-z])(?=.*[A-Z])/;
+    // Seleciona as caixas de senha
+    var senha1 = $('#password');
+    var senha2 = $('#password-2');
+  
+    // Função que valida as senhas
+    function validarSenha() {
+      if (senha1.val() === senha2.val()) {
+        // As senhas são iguais
+        senha1.addClass('is-valid');
+        senha2.addClass('is-valid');
+      } else {
+        // As senhas são diferentes
+        senha1.removeClass('is-valid');
+        senha2.removeClass('is-valid');
 
-    if($(this).val().length<8){
-      $(this).addClass('is-invalid');
-
-    }else{
-      $(this).removeClass('is-invalid');
-      $(this).addClass('is-valid');
-      
-    }   
+        senha1.addClass('is-invalid');
+        senha2.addClass('is-invalid');
+        
+      }
+    }
+  
+    // Adiciona o evento "input" às caixas de senha
+    senha1.on('input', validarSenha);
+    senha2.on('input', validarSenha);
   });
-});
+
+
+//|| 
+
+
+})();
+
+
+
+
+
+
 
   
 $(document).ready(function() {
